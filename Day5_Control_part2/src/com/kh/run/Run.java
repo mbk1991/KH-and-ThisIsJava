d8be5bc9dcbe6f6b2c7f1e4842593d050f9072eb
mabin <mbk1991@naver.com>
2022-06-17 11:37:48 +0900

    commit 2022_0614 11:37

--- Day5_Control_part2/src/com/kh/run/Run.java
+++ Day5_Control_part2/src/com/kh/run/Run.java
@@ -1,13 +1,19 @@
 package com.kh.run;
 
 import com.kh.exercixe.Exercise_Break_Continue;
+import com.kh.exercixe.Exercise_ForDouble;
 
 public class Run {
 
 	public static void main(String[] args) {
 		Exercise_Break_Continue ebc = new Exercise_Break_Continue();
-		ebc.exercise1();
+//		ebc.exercise1();
 //		ebc.exercise2();
+		
+		Exercise_ForDouble efd = new Exercise_ForDouble();
+//		efd.exercise1();
+//		efd.exercise2();
+		efd.exercise3();
 
 	}
 
