8f7b5de0d58b108b2c3b65e1f945e4fa779db8d1
mabin <mbk1991@naver.com>
2022-09-04 12:03:18 +0900

    220904 12:03 스프링연습

---
+++ SpringWorkSpace/diexam01/src/main/java/kr/or/connect/diexam01/Engine.java
@@ -0,0 +1,12 @@
+package kr.or.connect.diexam01;
+
+public class Engine {
+	
+	public Engine() {
+		System.out.println("Engine 생성자");
+	}
+
+	public void exec() {
+		System.out.println("엔진을 가동합니다.");
+	}
+}
