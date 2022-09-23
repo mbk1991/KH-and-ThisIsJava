2884911d38b0513bc70bc74ddf834872d1e8cac6
mabin <mbk1991@naver.com>
2022-09-23 12:29:35 +0900

    220923 12:29 문서수정:세미프로젝트문서

---
+++ JavaWorkSpace/Exercise/src/test/test/test/TestMainClass.java
@@ -0,0 +1,22 @@
+package test.test.test;
+
+public class TestMainClass {
+
+	public static void main(String[] args) {
+		
+		String target="<p><img src=\"/resources/image/review/summerImageFiles/20220919164712.png\" style=\"width: 543px;\"><img src=\"/resources/image/review/summerImageFiles/20220919164617.png\" style=\"width: 678px;\">"
+				+ "</p><p><img src=\"/resources/image/review/summerImageFiles/20220920085349.png\" style=\"width: 25%;\"><br></p><p>다른 컴퓨터에서는 이미지가 안 뜸."
+				+ "</p><p>이미지 저장 폴더가 공유되고 있지 않기 때문.</p>";
+		
+		String regEx="src";
+		
+		String[] sources = target.split(regEx);
+
+		int index = 0;
+		for(String source:sources) {
+			System.out.println(index+" : "+source);
+			System.out.println();
+			index++;
+		}
+	}
+}
